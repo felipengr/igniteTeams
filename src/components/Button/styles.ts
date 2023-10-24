e1ab3a79cc016e3cd60d9ac6d65dcdf2a68e2196
Felipe Nogueira <felipe.nogueira@maeztra.com>
2023-10-23 21:34:35 -0300

    componente input

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -15,6 +15,7 @@ export const Container = styled(TouchableOpacity) <Props>`
     border-radius: 6px;
     justify-content: center;
     align-items: center;
+    margin-top: 20px;
 `
 
 export const Title = styled.Text`
